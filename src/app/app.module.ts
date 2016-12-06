2bf157161b82a5cc1a9c3cdd640425415a1f8b9f
Ante <vuletic23@gmail.com>
2016-12-06 19:22:20 +0100

    Implement file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
-  ],
+    AppComponent, 
+    SearchComponent,
+    ProductListComponent,
+    CategoriesComponent
+],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
